68e2a2419a34d4467bd12c1720d1474b71846203
Chuck Smith <chuck@eclecticsaddlebag.com>
2019-08-20 12:15:06 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -11,7 +11,7 @@ def division(num1, num2)
 end
 
 def multiplication(num1, num2)
-  return num1 + num2
+  return num1 * num2
 end
 
 def modulo(num1, num2)
@@ -19,5 +19,6 @@ def modulo(num1, num2)
 end
 
 def square_root(num)
+  return num * num
 
 end
